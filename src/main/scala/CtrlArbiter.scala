be2d20a83f67000d65fb6e7fcaae4800941fab79
liushaopeng <liushaopeng@coinv.com>
2023-04-04 17:20:21 +0800

    [mod] 编写完成主控所有解析发送模块

--- src/main/scala/CtrlArbiter.scala
+++ src/main/scala/CtrlArbiter.scala
@@ -1,12 +1,13 @@
 import spinal.core._
 import spinal.lib._
+import spinal.lib.fsm._
 
 case class UnpackSmall() extends Component {
   val io = new Bundle {
 
     val Soure = slave(Stream(Bits(320 bits)))
 
-    val Sink = master(Stream(Bits(32 bits)))
+    val Sink = Vec(master(Stream(Bits(32 bits))), 4)
 
   }
 
@@ -15,6 +16,7 @@ case class UnpackSmall() extends Component {
   val PVaildNum = Bits(6 bits)
   val PType = Bits(4 bits)
   val FIF0 = Stream(Bits(32 bits))
+  val FIF02 = Stream(Bits(32 bits))
   val FIFO1 = Stream(Bits(320 bits))
   val PCnt = Reg(UInt(6 bits)) init (0)
 
@@ -35,7 +37,23 @@ case class UnpackSmall() extends Component {
     PCnt := PCnt + 1
   }
 
-  FIF0.queue(4).throwWhen(PCnt > PVaildNum.asUInt) >> io.Sink
+  FIF0.queue(4).throwWhen(PCnt > PVaildNum.asUInt) >> FIF02
+
+  switch(PType) {
+    is(0) {
+      FIF02 >> io.Sink(0)
+    }
+    is(1) {
+      FIF02 >> io.Sink(1)
+    }
+    is(2) {
+      FIF02 >> io.Sink(2)
+    }
+    is(3) {
+      FIF02 >> io.Sink(3)
+    }
+  }
+
 
 }
 
@@ -61,11 +79,82 @@ class BigPack() extends Component {
   val io = new Bundle {
     val Source = slave(Stream(Bits(32 bits)))
     val PLength = in Bits (32 bits)
-    val PTrigger = in Bits (4 bits)
+    //    val PTrigger = in Bits (4 bits)
+    val Sink = master(Stream(Bits(32 bits)))
   }
-  
+
+  val FIFO = Stream(Bits(32 bits))
+
+  val PHead = Vec(Bits(32 bits), 32)
+
+  val PTail = Vec(Bits(32 bits), 32)
+
+  io.Source.ready := True
+  io.Sink.valid := False
+  io.Sink.payload := 0
+  PHead.map(_ := 0)
+  PTail.map(_ := 0)
+
+  //  io.Source.queue(16) >> FIFO
+
+  val PackTxFsm = new StateMachine {
+
+    val HeadCnt = Reg(UInt(6 bits)) init (0)
+    val BodyCnt = Reg(UInt(32 bits)) init (0)
+    val TailCnt = Reg(UInt(6 bits)) init (0)
+
+    val SIdle: State = new State with EntryPoint {
+      whenIsActive {
+        HeadCnt := 0
+        BodyCnt := 0
+        goto(STxHead)
+      }
+    }
+    val STxHead: State = new State() {
+      whenIsActive {
+        when(io.Sink.ready === True && HeadCnt < 32) {
+          io.Sink.valid := True
+          io.Sink.payload := PHead(HeadCnt.resize(5))
+          HeadCnt := HeadCnt + 1
+        }
+        when(HeadCnt === 32) {
+          goto(STxBody)
+        }
+      }
+    }
+    val STxBody: State = new State() {
+      whenIsActive {
+        when(io.Sink.ready === True && BodyCnt < io.PLength.asUInt) {
+          io.Sink.valid := io.Source.valid
+          io.Sink.payload := io.Source.payload
+          BodyCnt := BodyCnt + 1
+        }
+        when(BodyCnt === io.PLength.asUInt) {
+          goto(STxTail)
+        }
+      }
+    }
+    val STxTail: State = new State() {
+      whenIsActive {
+        when(io.Sink.ready === True && TailCnt < 32) {
+          io.Sink.valid := True
+          io.Sink.payload := PHead(TailCnt.resize(5))
+          TailCnt := TailCnt + 1
+        }
+        when(TailCnt === 32) {
+          goto(SDone)
+        }
+      }
+    }
+    val SDone: State = new State() {
+      whenIsActive {
+        goto(SIdle)
+      }
+    }
+  }
 
 }
+
 
 object UnpackTop extends App {
   SpinalVerilog(new UnpackSmall())
